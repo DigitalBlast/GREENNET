b45f3f1406d2a6c22bf8ea06b10af1503f1d9730
Digital Blast <alexander.m.rankin@gmail.com>
2015-10-16 14:38:33 -0400

    Copyright

---
+++ api.lua
@@ -0,0 +1,23 @@
+--Made by DigitalBlast Team
+--[[
+	.By using this software, you agree that websites that have harmful, offensive content WILL BE REMOVED
+	and that domain name will be placeholded. (makeing it un-useable)
+	
+	.Also by useing this software you agree that your website will contain NO inapropreate content
+	
+	 Host ComputerCraft webpages
+    Copyright (C) 2014-2015  DigitalBlast (Used to be Sapphire)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]---
